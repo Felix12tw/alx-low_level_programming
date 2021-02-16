028e69aba0c45c30a7831b15ea12ca458d7e235d
Felix Twoli <felixtwoli21@gmail.com>
2021-02-16 21:31:36 +0000

    Update 9 stray copy file

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,4 @@
-nclude <stdio.h>
+#include <stdio.h>
 #include "holberton.h"
 
 /**
@@ -11,7 +11,7 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i<n; i++)
 	{
 		if (i == 0)
 			printf("%d", a[i]);
